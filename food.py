e770582ea8c7ce77e9a6f1f0432e5605e814135e
Angel Anton <Angelanton01@gmail.com>
2022-09-03 14:21:58 -0400

    version 1.1

--- food.py
+++ food.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from turtle import Turtle
 from random import randint
 
@@ -16,22 +15,3 @@ class Food(Turtle):
 
     def refresh(self):
         self.goto(x=randint(-280, 280), y=randint(-270, 280))
-=======
-from turtle import Turtle
-from random import randint
-
-
-class Food(Turtle):
-
-    def __init__(self):
-        super().__init__()
-        self.shape('circle')
-        self.penup()
-        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
-        self.color('red')
-        self.speed(0)
-        self.refresh()
-
-    def refresh(self):
-        self.goto(x=randint(-280, 280), y=randint(-280, 280))
->>>>>>> 4d27b43c4882086c218f83a99bf471b8a628f709
--- score.py
+++ score.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from turtle import Turtle
 ALIGN = 'center'
 FONT = ('Consolas', 15, 'normal')
@@ -43,29 +42,3 @@ class Score(Turtle):
                 file.write(str(self.high_score))
                 self.goto(x=0, y=-20)
                 self.write(arg='NEW HIGH SCORE!', align=ALIGN, font=FONT)
-=======
-from turtle import Turtle
-ALIGN = 'center'
-FONT = ('Consolas', 15, 'normal')
-
-
-class Score(Turtle):
-    def __init__(self):
-        super().__init__()
-        self.hideturtle()
-        self.penup()
-        self.goto(x=0, y=275)
-        self.score = 0
-        self.color('white')
-
-    def increment(self):
-        self.score += 1
-
-    def print_score(self):
-        self.clear()
-        self.write(arg=f'Score: {self.score}', align=ALIGN, font=FONT)
-
-    def game_over(self):
-        self.goto(x=0, y=0)
-        self.write(arg='GAME OVER', align=ALIGN, font=FONT)
->>>>>>> 4d27b43c4882086c218f83a99bf471b8a628f709
